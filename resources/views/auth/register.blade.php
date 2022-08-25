3d4f6c5ff051e566396439fc64a7b0b1b0ee1b3d
musheabdulhakim <musheabdulhakim99@gmail.com>
2022-08-25 00:44:45 +0000

    social logins removed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -132,24 +132,6 @@
             @endif
           </p>
 
-          <div class="divider my-2">
-            <div class="divider-text">or</div>
-          </div>
-
-          <div class="auth-footer-btn d-flex justify-content-center">
-            <a href="#" class="btn btn-facebook">
-              <i data-feather="facebook"></i>
-            </a>
-            <a href="#" class="btn btn-twitter white">
-              <i data-feather="twitter"></i>
-            </a>
-            <a href="#" class="btn btn-google">
-              <i data-feather="mail"></i>
-            </a>
-            <a href="#" class="btn btn-github">
-              <i data-feather="github"></i>
-            </a>
-          </div>
         </div>
       </div>
       <!-- /Register basic -->
