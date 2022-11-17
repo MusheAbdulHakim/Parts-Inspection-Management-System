8cb6cfc6af7a9f448414f60fe465e946b2f4d8ee
musheabdulhakim <musheabdulhakim99@gmail.com>
2022-11-17 21:36:21 +0000

    user permissions

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 use Spatie\Permission\Models\Role;
@@ -95,6 +96,41 @@ class RolesController extends Controller
         ]);
         $role->syncPermissions($request->permission);
         return redirect()->route('roles.index')->with(notify('Role updated successfully'));
+    }
+
+    public function userPermissions(Request $request){
+        if($request->ajax()){
+            $user_permissions = User::with('permissions')->get();
+            return DataTables::of($user_permissions)
+                ->addIndexColumn()
+                ->addColumn('user', function($row){
+                    return $row->name;
+                })
+                ->addColumn('permissions',function ($user){
+                    return array_map(function($permission){
+                        return $permission['name'];
+                    },$user->getAllPermissions()->toArray());
+                })
+                ->addColumn('action',function ($row){
+                    $editbtn = '<a href="javascript:void(0)" data-id="'.$row->id.'" data-user="'.$row->id.'" data-permissions='.($row->getAllPermissions()->pluck('name')).' class="edit"><button class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></button></a>';
+                    if(!can('edit-role')){
+                        $editbtn = '';
+                    }
+                    $btn = $editbtn;
+                    return $btn;
+                })
+                ->rawColumns(['action','permissions'])
+                ->make(true);
+        }
+        $users = User::get();
+        $permissions = Permission::get();
+        return view('admin.roles.user-permissions',compact(
+            'users','permissions'
+        ));
+    }
+
+    public function updateUserPermissions(Request $request){
+        
     }
 
     /**
---
+++ resources/views/admin/roles/user-permissions.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.contentLayoutMaster')
+
+@section('title', 'Assign Permissions')
+
+<x-assets.datatables />
+
+@section('page-style')
+  <!-- Page css files -->
+  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+  <link rel="stylesheet" href="{{asset('vendors/css/forms/select/select2.min.css')}}">
+@endsection
+
+
+@section('content')
+
+    <!-- Permission Table -->
+    <div class="card">
+        <div class="p-2">
+            <div class="card-datatable table-responsive">
+                <table id="datatable" class="datatables-permissions table">
+                    <thead class="table-light">
+                    <tr>
+                        <th>User</th>
+                        <th>Permissions</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+  <!--/ Permission Table -->
+@endsection
+
+@push('modals')
+    
+    <!-- Edit Permission Modal -->
+    <div class="modal fade" id="editPermissionModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-transparent">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-3 pt-0">
+            <div class="text-center mb-2">
+                <h1 class="mb-1">Edit Permissions</h1>
+                <p>Edit permission as per your requirements.</p>
+            </div>
+            <form method="post" action="{{route('user-permissions.update')}}" class="row">
+                @csrf
+                @method("PUT")
+                <div class="col-12 mb-1">
+                    <label class="form-label" for="edit_user">User</label>
+                    <select data-placeholder="select user" name="user" id="edit_user" class="form-control">
+                        <option value=""></option>
+                        @foreach ($users as $user)
+                            <option value="{{$user->id}}">{{$user->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-12">
+                    <label class="form-label" for="edit_permissions">Permissions</label>
+                    <select data-placeholder="select permissions" multiple name="permissions[]" id="edit_permissions" class="form-control select2">
+                        <option value=""></option>
+                        @foreach ($permissions as $permission)
+                            <option>{{$permission->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-sm-3 ps-sm-0">
+                    <button type="submit" class="btn btn-primary mt-2">Update</button>
+                </div>
+                
+            </form>
+            </div>
+        </div>
+        </div>
+    </div>
+    <!--/ Edit Permission Modal -->
+@endpush
+
+@push('vendor-script')
+<script src="{{asset(mix('vendors/js/forms/select/select2.full.min.js'))}}"></script>   
+@endpush
+
+@section('page-script')
+<script src="{{ asset(mix('vendors/js/forms/validation/jquery.validate.min.js')) }}"></script>
+
+<script>
+    $(document).ready(function(){
+        var table = $('#datatable').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{route('user-permissions.index')}}",
+            columns: [
+                {data: 'user', name: 'user'},
+                {data: 'permissions', name: 'permissions'},
+                {data: 'action', name: 'action', orderable: false, searchable: false},
+            ]
+            
+        });
+        $('#datatable').on('click','.edit',function(){
+            var id = $(this).data('id');
+            var user = $(this).data('user');
+            var permissions = $(this).data('permissions');
+            $('#editPermissionModal').modal('show');
+            $('#edit_user').val(user).trigger('change');
+            $('#edit_permissions').val(permissions).trigger('change');
+        });
+    });
+</script>
+@endsection
