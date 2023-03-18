7684740c379b575a1955b9b5745873592259534d
musheabdulhakim <musheabdulhakim99@gmail.com>
2023-03-18 07:55:51 +0000

    users widget added

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
         $pageConfigs = ['pageHeader' => false];
+        $users_count = User::count();
         return view('admin.dashboard',compact(
-            'pageConfigs'
+            'pageConfigs','users_count'
         ));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,16 +4,17 @@
 
 @section('content')
 <div class="row">
-  <div class="col-xl-2 col-md-4 col-sm-6">
+  <div class="col-xl-4 col-md-4 col-sm-6">
       <div class="card text-center">
           <div class="card-body">
-              <div class="avatar bg-light-info p-50 mb-1">
-                  <div class="avatar-content">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye font-medium-5"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                  </div>
-              </div>
-              <h2 class="fw-bolder">36.9k</h2>
-              <p class="card-text">Views</p>
+              <a href="{{route('users.index')}}"><div class="avatar bg-light-info p-50 mb-1">
+                    <div class="avatar-content">
+                        <i data-feather="users"></i>
+                    </div>
+            </div>
+            </a>
+              <h2 class="fw-bolder">{{$users_count}}</h2>
+              <p class="card-text">Users</p>
           </div>
       </div>
   </div>
