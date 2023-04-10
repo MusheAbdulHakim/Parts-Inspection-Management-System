6c91d56b5f0a5d655a3cd9b030d46428a31c0c65
musheabdulhakim <musheabdulhakim99@gmail.com>
2023-04-10 05:17:23 -0700

    initial commit

---
+++ deploy.php
@@ -0,0 +1,74 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'recipe/rsync.php';
+
+
+// Config
+
+set('repository', 'https://github.com/MusheAbdulHakim/Parts-Inspection-Management-System.git');
+
+set('ssh_multiplexing', true); // Speed up deployment
+
+set('rsync_src', function () {
+    return __DIR__; // If your project isn't in the root, you'll need to change this.
+});
+
+// Configuring the rsync exclusions.
+// You'll want to exclude anything that you don't want on the production server.
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/storage/',
+        '/vendor/',
+        '/node_modules/',
+        '.github',
+        'deploy.php',
+    ],
+]);
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host(' altalus.online') // Name of the server
+    ->hostname('104.243.34.13') // Hostname or IP address
+    ->stage('production') // Deployment stage (production, staging, etc)
+    ->user('deploy') // SSH user
+    ->set('deploy_path', '/home/altalus1/pims.altalus.online/public'); // Deploy path
+
+// Set up a deployer task to copy secrets to the server.
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock'); // Unlock after failed deploy
+
+desc('Deploy the application');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync', // Deploy code & built assets
+    'deploy:secrets', // Deploy secrets
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link', // |
+    'artisan:view:cache',   // |
+    'artisan:config:cache', // | Laravel specific steps
+    'artisan:optimize',     // |
+    'artisan:migrate --seed',      // |
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
